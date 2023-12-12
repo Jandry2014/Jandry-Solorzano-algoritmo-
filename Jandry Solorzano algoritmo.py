2718d3148695b031f7870f0b2f787a22193048eb
Jandry2014 <153627990+Jandry2014@users.noreply.github.com>
2023-12-12 11:14:58 -0500

    Add files via upload

---
+++ Jandry Solorzano algoritmo.py
@@ -0,0 +1,64 @@
+# Suma dos números enteros
+num1 = 120
+num2 = 230
+suma = num1 + num2
+print("La suma de", num1, "y", num2, "es:", suma)
+
+# Resta dos números de punto flotante
+num3 = 30.5
+num4 = 9.5
+resta = num3 - num4
+print("La resta de", num3, "y", num4, "es:", resta)
+
+# Multiplica dos números enteros
+num5 = 16
+num6 = 70
+mult = num5 * num6
+print("La multiplicación de", num5, "y", num6, "es:", mult)
+
+# Divide dos números de punto flotante
+num7 = 15.3
+num8 = 9.4
+division = num7 / num8
+print("La división de", num7, "y", num8, "es:", division)
+# Nombre completo
+nombre_completo = "Jandry Solorzano"
+
+# Longitud del nombre completo
+longitud_nombre = len(nombre_completo)
+
+# Imprime en pantalla
+print("El nombre completo es:", nombre_completo)
+print("La longitud del nombre completo es:", longitud_nombre)
+# Conversión de tipos de datos
+num_entero = 80
+num_flotante = 80.9
+
+# Conversión entero a flotante
+num_flotante_entero = float(num_entero)
+print("La conversión de", num_entero, "a flotante es:", num_flotante_entero)
+
+# Conversión flotante a entero
+num_entero_flotante = int(num_flotante)
+print("La conversión de", num_flotante, "a entero es:", num_entero_flotante)
+# Comentarios y documentación en el código
+
+# Operaciones Básicas y Tipos de Datos
+num1 = 120
+num2 = 230
+suma = num1 + num2
+print("La suma de", num1, "y", num2, "es:", suma)
+
+# Manipulación de Cadenas de Texto
+nombre_completo = "Jandry Solorzano"
+longitud_nombre = len(nombre_completo)
+print("El nombre completo es:", nombre_completo)
+print("La longitud del nombre completo es:", longitud_nombre)
+
+# Conversión de Tipos de Datos
+num_entero = 80
+num_flotante = 80.9
+num_flotante_entero = float(num_entero)
+print("La conversión de", num_entero, "a flotante es:", num_flotante_entero)
+num_entero_flotante = int(num_flotante)
+print("La conversión de", num_flotante, "a entero es:", num_entero_flotante)
